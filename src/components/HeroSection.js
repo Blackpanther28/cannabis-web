7f4a4de4c59b9c874048d7bff239f6827032a7cd
Blackpanther28 <phongthep.p@bemplc.co.th>
2021-07-27 17:06:52 +0700

    gggg

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,15 +6,9 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
-      <h1>Cannabis chain a farm the world's greenest coin</h1>
-      <p>
-        A 100% decentralized and eco-friendly blockchain that enables everyone
-        to farm by using their free disk storage. We think farming is the future
-        of Cryptocurrencies. Cannabis’s blockchain implements Proof of Space and
-        Time (PoST) instead of Proof of Work (PoW), drastically reducing energy
-        consumption.
-      </p>
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
       <div className="hero-btns">
         <Button
           className="btns"
@@ -23,21 +17,14 @@ function HeroSection() {
         >
           GET STARTED
         </Button>
-        <a
-          href="https://github.com/CannabisChain"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn-mobile"
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          onClick={console.log("hey")}
         >
-          <Button
-            className="btns"
-            buttonStyle="btn--primary"
-            buttonSize="btn--large"
-            onClick={console.log("hey")}
-          >
-            View on Github
-          </Button>
-        </a>
+          WATCH TRAILER <i className="far fa-play-circle" />
+        </Button>
       </div>
     </div>
   );
